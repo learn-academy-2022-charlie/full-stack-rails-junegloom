765b6eb89a68889e80ad14e22419801ce2fb73c3
jennerdulce <jennerdulce@gmail.com>
2022-06-16 11:49:27 -0700

    completed edit

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -25,6 +25,16 @@ class PostController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
     private 
     def post_params
         params.require(:post).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+  root 'post#index'
   get '/posts' => 'post#index', as:'posts' 
+  get '/posts/:id' => 'post#show', as:'post'
+
+  # Clicking submit on the form on the new.html.erb will go directly to the /posts route and trigger the #create method
+  # These two go hand in hand
   get '/posts/new' => 'post#new', as: 'new_post'
   post '/posts'=> 'post#create'
-  get '/posts/:id' => 'post#show', as:'post'
+  # ===========
+
+  # Clicking submit on the form on the edit.html.erb will go directly to the PATCH /posts/:id route and trigger the #update method
+  # These two go hand in hand
+  get 'posts/:id/edit', to: 'post#edit', as: 'edit_post'
+  patch 'posts/:id', to: 'post#update'
+  # ===========
+
   delete '/posts/:id' => 'post#destroy', as: 'delete_post'
-  root 'post#index'
+  
 end
